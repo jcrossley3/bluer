94b649967eaf2a223b9acdee449ca01bdfb4d3b3
Dejan Bosanac <dejan@sensatic.net>
2022-06-02 09:26:44 -0400

    Add BT mesh element control and example how to receive messages

--- bluer/src/mesh/mod.rs
+++ bluer/src/mesh/mod.rs
@@ -5,7 +5,7 @@ pub mod network;
 mod types;
 pub use types::*;
 
-use crate::{method_call, Result, SessionInner};
+use crate::{method_call, Result, SessionInner, Error, ErrorKind, ERR_PREFIX};
 use dbus::{
     arg::{PropMap, RefArg, Variant},
     nonblock::{stdintf::org_freedesktop_dbus::ObjectManager, Proxy, SyncConnection},
@@ -16,11 +16,23 @@ use drogue_device::drivers::ble::mesh::{
     address::{Address, UnicastAddress},
     app::ApplicationKeyIdentifier,
     pdu::{
-        access::{AccessMessage, AccessPayload},
-        ParseError,
+        access::{AccessPayload},
     },
 };
-use std::{collections::HashMap, sync::Arc, time::Duration};
+use std::{
+    collections::HashMap,
+    fmt,
+    num::NonZeroU16,
+    pin::Pin,
+    sync::{Arc},
+    task::Poll,
+    time::Duration,
+};
+use pin_project::pin_project;
+use tokio::sync::{mpsc, watch};
+use tokio_stream::wrappers::ReceiverStream;
+use futures::{Stream};
+use strum::{IntoStaticStr};
 
 pub(crate) const SERVICE_NAME: &str = "org.bluez.mesh";
 pub(crate) const PATH: &str = "/org/bluez/mesh";
@@ -43,6 +55,8 @@ type ElementConfig = HashMap<String, Variant<Box<dyn RefArg + 'static>>>;
 pub struct Element {
     /// Element models
     pub models: Vec<Box<dyn Model>>,
+    /// Control handle for element once it has been registered.
+    pub control_handle: Option<ElementControlHandle>,
 }
 
 /// An element exposed over D-Bus to bluez.
@@ -64,31 +78,6 @@ impl RegisteredElement {
     dbus_interface!();
     dbus_default_interface!(ELEMENT_INTERFACE);
 
-    fn message_received(
-        &self, source: u16, key_index: u16, destination: Variant<Box<dyn RefArg + 'static>>, data: Vec<u8>,
-    ) -> core::result::Result<(), ParseError> {
-        log::trace!(
-            "Message received for element {:?}: (source: {:?}, key_index: {:?}, dest: {:?}, data: {:?})",
-            self.index,
-            source,
-            key_index,
-            destination,
-            data
-        );
-
-        // let key = ApplicationKeyIdentifier::from(u8::try_from(key_index).unwrap_or_default());
-        // let src: UnicastAddress = source.try_into()?;
-        // // TODO handle virtual addresses
-        // let value = &destination.0;
-        // let dest = Address::parse(dbus::arg::cast::<u16>(value).unwrap().to_be_bytes());
-        // let payload = AccessPayload::parse(&data)?;
-
-        // let message = AccessMessage::new(key, src, dest, payload);
-
-        // log::info!("AccessMessage {:?}", message);
-        Ok(())
-    }
-
     pub(crate) fn register_interface(cr: &mut Crossroads) -> IfaceToken<Arc<Self>> {
         cr.register(ELEMENT_INTERFACE, |ib: &mut IfaceBuilder<Arc<Self>>| {
             ib.method_with_cr_async(
@@ -104,7 +93,37 @@ impl RegisteredElement {
                     Vec<u8>,
                 )| {
                     method_call(ctx, cr, move |reg: Arc<Self>| async move {
-                        reg.message_received(source, key_index, destination, data);
+                        log::trace!(
+                            "Message received for element {:?}: (source: {:?}, key_index: {:?}, dest: {:?}, data: {:?})",
+                            reg.index,
+                            source,
+                            key_index,
+                            destination,
+                            data
+                        );
+
+                        let key = ApplicationKeyIdentifier::from(u8::try_from(key_index).unwrap_or_default());
+                        let src: UnicastAddress = source.try_into().map_err(|_| ReqError::Failed)?;
+                        // TODO handle virtual addresses
+                        let value = &destination.0;
+                        let dest = Address::parse(dbus::arg::cast::<u16>(value).unwrap().to_be_bytes());
+                        let payload = AccessPayload::parse(&data).map_err(|_| ReqError::Failed)?;
+
+                        let msg = ElementMessage {
+                            key, src, dest, payload
+                        };
+
+                        match &reg.element.control_handle {
+                            Some(handler) => {
+                                handler
+                                .messages_tx
+                                .send(msg)
+                                .await
+                                .map_err(|_| ReqError::Failed)?;
+                            }
+                            None => ()
+                        }
+
                         Ok(())
                     })
                 },
@@ -135,3 +154,113 @@ impl RegisteredElement {
         })
     }
 }
+
+/// An object to control a element and receive messages once it has been registered.
+///
+/// Use [element_control] to obtain controller and associated handle.
+#[pin_project]
+pub struct ElementControl {
+    handle_rx: watch::Receiver<Option<NonZeroU16>>,
+    #[pin]
+    messages_rx: ReceiverStream<ElementMessage>,
+}
+
+impl fmt::Debug for ElementControl {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ElementControl {{ handle: {} }}", self.handle().map(|h| h.get()).unwrap_or_default())
+    }
+}
+
+impl ElementControl {
+    /// Gets the assigned handle of the element.
+    pub fn handle(&self) -> crate::Result<NonZeroU16> {
+        match *self.handle_rx.borrow() {
+            Some(handle) => Ok(handle),
+            None => Err(Error::new(ErrorKind::NotRegistered)),
+        }
+    }
+}
+
+impl Stream for ElementControl {
+    type Item = ElementMessage;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut std::task::Context) -> Poll<Option<Self::Item>> {
+        self.project().messages_rx.poll_next(cx)
+    }
+}
+
+/// A handle to store inside a element definition to make it controllable
+/// once it has been registered.
+///
+/// Use [element_control] to obtain controller and associated handle.
+pub struct ElementControlHandle {
+    handle_tx: watch::Sender<Option<NonZeroU16>>,
+    messages_tx: mpsc::Sender<ElementMessage>,
+}
+
+impl Default for ElementControlHandle {
+    fn default() -> Self {
+        Self { handle_tx: watch::channel(None).0, messages_tx: mpsc::channel(1).0 }
+    }
+}
+
+impl fmt::Debug for ElementControlHandle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "ElementControlHandle")
+    }
+}
+
+/// Creates a [ElementControl] and its associated [ElementControlHandle].
+///
+/// Keep the [ElementControl] and store the [ElementControlHandle] in [Element::control_handle].
+pub fn element_control() -> (ElementControl, ElementControlHandle) {
+    let (handle_tx, handle_rx) = watch::channel(None);
+    let (messages_tx, messages_rx) = mpsc::channel(1);
+    (
+        ElementControl { handle_rx, messages_rx: ReceiverStream::new(messages_rx) },
+        ElementControlHandle { handle_tx, messages_tx },
+    )
+}
+
+// ===========================================================================================
+// Request error
+// ===========================================================================================
+
+/// Error response from us to a Bluetooth request.
+#[derive(Clone, Copy, Debug, displaydoc::Display, Eq, PartialEq, Ord, PartialOrd, Hash, IntoStaticStr)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[non_exhaustive]
+pub enum ReqError {
+    /// Bluetooth request failed
+    Failed,
+    /// Bluetooth request already in progress
+    InProgress,
+    /// Invalid offset for Bluetooth GATT property
+    InvalidOffset,
+    /// Invalid value length for Bluetooth GATT property
+    InvalidValueLength,
+    /// Bluetooth request not permitted
+    NotPermitted,
+    /// Bluetooth request not authorized
+    NotAuthorized,
+    /// Bluetooth request not supported
+    NotSupported,
+}
+
+impl std::error::Error for ReqError {}
+
+impl Default for ReqError {
+    fn default() -> Self {
+        Self::Failed
+    }
+}
+
+impl From<ReqError> for dbus::MethodErr {
+    fn from(err: ReqError) -> Self {
+        let name: &'static str = err.into();
+        Self::from((ERR_PREFIX.to_string() + name, &err.to_string()))
+    }
+}
+
+/// Result of a Bluetooth request to us.
+pub type ReqResult<T> = std::result::Result<T, ReqError>;
